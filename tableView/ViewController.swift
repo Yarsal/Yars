f6910f0f3d99fde42f3965fbded2f638cf1acdf1
Customer <customer@Customers-MacBook-Pro.local>
2017-11-22 12:28:04 +0300

    my comment

--- tableView/ViewController.swift
+++ tableView/ViewController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    // теперь создаем массив список покупок
+    let list = ["Milk", "Honey", "Bread", "Takos", "Tomatoes"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(list.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // теперь нужно определить содержание клетки
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return(cell)
+    }
+    
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
